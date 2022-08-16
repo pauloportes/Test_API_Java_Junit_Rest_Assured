f68af04d91f3930803068e567103fb1c0aa0641d
paulo.portes <pauloa.portes@iteris.com.br>
2022-08-16 15:33:33 -0300

    test(automation) test class fix

--- src/test/java/dev/restapi/suite/Test_Suite.java
+++ src/test/java/dev/restapi/suite/Test_Suite.java
@@ -4,25 +4,25 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import dev.codescreen.Delete;
-import dev.codescreen.FooTest;
-import dev.codescreen.Get_Single_User;
-import dev.codescreen.Get_Single_User_Not_Found;
-import dev.codescreen.Post_Create;
-import dev.codescreen.Post_Login_Successful;
-import dev.codescreen.Post_Login_Unsuccessful;
-import dev.codescreen.Put_Update;
+import dev.restapi.Delete;
+import dev.restapi.Get_List_Users;
+import dev.restapi.Get_Single_User;
+import dev.restapi.Get_Single_User_Not_Found;
+import dev.restapi.Post_Create;
+import dev.restapi.Post_Login_Successful;
+import dev.restapi.Post_Login_Unsuccessful;
+import dev.restapi.Put_Update;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	FooTest.class,
 	Get_Single_User.class,
 	Get_Single_User_Not_Found.class,
 	Post_Create.class,
 	Put_Update.class,
 	Delete.class,
 	Post_Login_Successful.class,
-	Post_Login_Unsuccessful.class,	
+	Post_Login_Unsuccessful.class,
+	Get_List_Users.class
 })
 public class Test_Suite {
 
